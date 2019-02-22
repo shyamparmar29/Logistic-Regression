a4fbe2f03d393cd90345ff25a0acf23073cc23f4
Shyam Parmar <46047509+shyamparmar29@users.noreply.github.com>
2019-02-22 19:35:35 +0530

    Update Iris_skl.py

--- Iris_skl.py
+++ Iris_skl.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 22 19:12:25 2019
-
-@author: Shyam Parmar
-"""
+#Implementation of logistic regression by skl 
 
 import numpy as np
 import pandas as pd
